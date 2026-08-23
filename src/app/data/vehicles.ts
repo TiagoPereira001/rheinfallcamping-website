c6ef754cf5168b56aff7c7fb93160e15d0bb8723
Tiago Pereira Dias <tiagopereiradias@MBP-de-Tiago.lan>
2026-08-24 00:29:28 +0100

    update na segurança

--- src/app/data/vehicles.ts
+++ src/app/data/vehicles.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
   { id: "home", label: "Início", path: "/" },
   { id: "stock", label: "Stock", path: "/stock" },
-  { id: "vender", label: "Vender a minha", path: "/vender" },
+  { id: "vender", label: "Quer vender a sua autocaravana?", path: "/vender" },
   { id: "contactos", label: "Contactos", path: "/contactos" },
 ];
